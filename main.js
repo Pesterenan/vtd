6fd0c91229ef3b9577ea51a9c498414210d7b715
Pesterenan <pesterenan@gmail.com>
2024-04-18 21:52:15 -0300

    feat: started with Electron app tutorial

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+}).catch((error) => {
+    console.error(`DEU ERRO VÉI :( ${error.message}`);
+  });
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
