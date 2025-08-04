a331935b96aaceef1abd4907627741c98232eeaf
Pesterenan <pesterenan@gmail.com>
2025-08-04 11:14:19 -0300

    feat: new static bounding box method

--- src/utils/boundingBox.ts
+++ src/utils/boundingBox.ts
@@ -1,4 +1,5 @@
-import type { Position, Size } from "src/components/types";
+import type { Position, Size, TElementData } from "src/components/types";
+import type { Element } from "src/components/elements/element";
 import { rotatePoint } from "./transforms";
 import { Vector } from "./vector";
 
@@ -102,5 +103,43 @@ export class BoundingBox {
       unrotatedBottomRight.y >= minBound.y &&
       unrotatedBottomRight.y <= maxBound.y
     );
+  }
+
+  public static calculateBoundingBox(elements: Array<Element<TElementData>>): {
+    position: Position;
+    size: Size;
+  } {
+    let minX = Number.POSITIVE_INFINITY;
+    let minY = Number.POSITIVE_INFINITY;
+    let maxX = Number.NEGATIVE_INFINITY;
+    let maxY = Number.NEGATIVE_INFINITY;
+
+    for (const element of elements) {
+      const boundingBox = element.getBoundingBox();
+
+      // Considerando os quatro pontos da BoundingBox
+      const corners = [
+        boundingBox.topLeft,
+        boundingBox.topRight,
+        boundingBox.bottomLeft,
+        boundingBox.bottomRight,
+      ];
+
+      // Atualizando os limites da bounding box que vai circundar todos os elementos
+      for (const corner of corners) {
+        if (corner.x < minX) minX = corner.x;
+        if (corner.y < minY) minY = corner.y;
+        if (corner.x > maxX) maxX = corner.x;
+        if (corner.y > maxY) maxY = corner.y;
+      }
+    }
+
+    // Calcula a posição e tamanho da TransformBox em torno de todos os elementos
+    const width = maxX - minX;
+    const height = maxY - minY;
+    return {
+      position: { x: minX + width / 2, y: minY + height / 2 },
+      size: { width, height },
+    };
   }
 }
