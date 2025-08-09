34f2c095e0156713ad1a2322056a38551872e114
Pesterenan <pesterenan@gmail.com>
2025-08-09 00:39:06 -0300

    fix: release file using a different gha

--- forge.config.ts
+++ forge.config.ts
@@ -15,15 +15,37 @@ const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
     icon: "resources/icon",
+    executableName: "vtd",
   },
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({
+      authors: "Renan Torres",
+      description: "Editor de miniaturas para vídeos.",
+      name: "vtd",
+      setupExe: "vtd-setup.exe",
       setupIcon: "resources/icon.ico",
+      setupMsi: "vtd-installer.msi",
     }),
     new MakerZIP({}, ["darwin"]),
-    new MakerRpm({}),
-    new MakerDeb({ options: { icon: "resources/icon.png" } }),
+    new MakerRpm({
+      options: {
+        name: "vtd",
+        genericName: "Video Thumbnail Designer",
+        homepage: "https://github.com/Pesterenan/vtd",
+      },
+    }),
+    new MakerDeb({
+      options: {
+        genericName: "Video Thumbnail Designer",
+        homepage: "https://github.com/Pesterenan/vtd",
+        icon: "resources/icon.png",
+        maintainer: "Renan Torres",
+        name: "vtd",
+        priority: "optional",
+        section: "video",
+      },
+    }),
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
@@ -53,8 +75,6 @@ const config: ForgeConfig = {
         ],
       },
     }),
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
