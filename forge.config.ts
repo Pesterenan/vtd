7de7389ef838c14e034bb344100fbdd9291121de
Pesterenan <pesterenan@gmail.com>
2025-08-08 22:03:31 -0300

    fix: removing build from release file for linux

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,8 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-// import { MakerZIP } from "@electron-forge/maker-zip";
-// import { MakerDeb } from "@electron-forge/maker-deb";
-// import { MakerRpm } from "@electron-forge/maker-rpm";
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerRpm } from "@electron-forge/maker-rpm";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
@@ -21,10 +21,9 @@ const config: ForgeConfig = {
     new MakerSquirrel({
       setupIcon: "resources/icon.ico",
     }),
-    // FIX: MAKE LINUX RELEASE WORK
-    // new MakerZIP({}, ["darwin"]),
-    // new MakerRpm({}),
-    // new MakerDeb({ options: { icon: "resources/icon.png" } }),
+    new MakerZIP({}, ["darwin"]),
+    new MakerRpm({}),
+    new MakerDeb({ options: { icon: "resources/icon.png" } }),
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
