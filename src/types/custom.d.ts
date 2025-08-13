0fc9c23d6a0e97dbd58e4369faa0db4ef56c04c2
Pesterenan <pesterenan@gmail.com>
2025-08-12 23:21:24 -0300

    feat: include app version on webpack config

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,4 +1,7 @@
-declare module "*.svg" {
-  const content: string;
+declare const APP_VERSION: string;
+
+declare module '*.svg' {
+  const content: React.FC<React.SVGProps<SVGSVGElement>>;
   export default content;
 }
+
